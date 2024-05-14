a2e8de3357643389533f4b0a48fa35e5240dfa1e
hyunkk1 <bbang970723@naver.com>
2024-05-14 12:16:23 +0900

    로그인 페이지 업로드

---
+++ minifront/src/Login/Login.jsx
@@ -0,0 +1,122 @@
+import React, { useEffect, useState } from "react";
+import Logoimg from "../images/로고.png";
+import "../Login/LoginIndex.css";
+
+const User = {
+  email: "test@naver.com",
+  pw: "test1234!",
+};
+
+function Login() {
+  const [email, setEmail] = useState("");
+  const [pw, setPw] = useState("");
+
+  const [emailValid, setEmailValid] = useState(false);
+  const [pwValid, setPwValid] = useState(false);
+  const [notAllow, setNotAllow] = useState(true);
+
+  const Clear = () => {
+    setEmail("");
+    setPw("");
+    setNotAllow(true);
+  };
+
+  useEffect(() => {
+    if (emailValid && pwValid) {
+      setNotAllow(false);
+      return;
+    }
+    setNotAllow(true);
+  }, [emailValid, pwValid]);
+
+  const handleEmail = (e) => {
+    setEmail(e.target.value);
+    const regex =
+      /^(([^<>()\[\].,;:\s@"]+(\.[^<>()\[\].,;:\s@"]+)*)|(".+"))@(([^<>()[\].,;:\s@"]+\.)+[^<>()[\].,;:\s@"]{2,})$/i;
+    if (regex.test(e.target.value)) {
+      setEmailValid(true);
+    } else {
+      setEmailValid(false);
+    }
+  };
+
+  const handlePw = (e) => {
+    setPw(e.target.value);
+    const regex =
+      /^(?=.*[a-zA-z])(?=.*[0-9])(?=.*[$`~!@$!%*#^?&\\(\\)\-_=+])(?!.*[^a-zA-z0-9$`~!@$!%*#^?&\\(\\)\-_=+]).{8,20}$/;
+    if (regex.test(e.target.value)) {
+      setPwValid(true);
+    } else {
+      setPwValid(false);
+    }
+  };
+  // 확인버튼
+  const onClickConfirmButton = () => {
+    if (email === User.email && pw === User.pw) {
+      alert("로그인에 성공했습니다.");
+    } else {
+      alert("등록되지 않은 회원입니다.");
+    }
+  };
+
+  return (
+    <div className="page">
+      <img src={Logoimg} className="Logoimg" alt="로고" />
+      <div className="titleWrap">이메일과 비밀번호를 입력해주세요</div>
+      <br />
+      <div className="contentWrap">
+        <div className="inputTitle">이메일 주소</div>
+        <div className="inputWrap">
+          <input
+            className="input"
+            type="text"
+            placeholder="test@gmail.com"
+            value={email}
+            onChange={handleEmail}
+          />
+        </div>
+        <div className="errorMessageWrap">
+          {!emailValid && email.length > 0 && (
+            <div>올바른 이메일을 입력해주세요.</div>
+          )}
+        </div>
+
+        <div style={{ marginTop: "26px" }} className="inputTitle">
+          비밀번호
+        </div>
+        <div className="inputWrap">
+          <input
+            className="input"
+            type="password"
+            placeholder="영문, 숫자, 특수문자 포함 8자 이상"
+            value={pw}
+            onChange={handlePw}
+          />
+        </div>
+
+        <div>
+          <button onClick={Clear} className="ClearButton">
+            초기화
+          </button>
+        </div>
+
+        <div className="errorMessageWrap">
+          {!pwValid && pw.length > 0 && (
+            <div>영문, 숫자, 특수문자 포함 8자 이상 입력해주세요.</div>
+          )}
+        </div>
+      </div>
+
+      <div>
+        <button
+          onClick={onClickConfirmButton}
+          disabled={notAllow}
+          className="bottomButton"
+        >
+          확인
+        </button>
+      </div>
+    </div>
+  );
+}
+export default Login;
