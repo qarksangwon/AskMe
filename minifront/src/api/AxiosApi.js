e1f3076a93ae78c764623969ef32482a61cb7372
qarksangwon <0629psw@gmail.com>
2024-05-19 23:28:48 +0900

    채팅방 내용 가져오는 axios 객체 생성

--- minifront/src/api/AxiosApi.js
+++ minifront/src/api/AxiosApi.js
@@ -5,6 +5,11 @@ const AxiosApi = {
   boardMain: async () => {
     return await axios.get(Askme_Domain + "/askme/board");
   },
+
+  //채팅방 번호에 따라서 내용을 가져올 수 있도록 요청을 보냄
+  chatContent: async (roomId) => {
+    return await axios.get(Askme_Domain + "/askme/chat", { roomId });
+  },
 };
 
 export default AxiosApi;
